ac0145c8e86ab8644c2ec82445d4d01ca6d50bdf
Egor Sozonov <Egor.Sozonov.09@gmail.com>
2022-03-16 12:39:43 +0300

    Rust: lexer expr equality

--- src/LexerTypes.rs
+++ src/LexerTypes.rs
@@ -5,33 +5,74 @@ pub enum Expr<'a> {
     stringToken(&'a[u8]),
     commentToken(&'a[u8]),
     operatorToken([Option<OperatorSymb>; 3]),
-    listExpr(Box<Expr<'a>>),
+    listExpr(ListExpr<'a>),
 }
 
+struct ListExpr<'a> {
+    val: Box<Vec<Expr<'a>>>,
+    pType: ExprLexicalType,
+}
+
+use Expr::*;
 
 impl<'a, 'b> PartialEq for Expr<'b> {
     fn eq(&self, other: &Self) -> bool {
         match(self, other) {
-            (Expr::intToken(a), Expr::intToken(b)) => a == b,
-            (Expr::floatToken(a), Expr::floatToken(b)) => a == b,
-            (Expr::wordToken(a), Expr::wordToken(b)) => a == b,
-            (Expr::stringToken(a), Expr::stringToken(b)) => a == b,
-            (Expr::commentToken(a), Expr::commentToken(b)) => a == b,
-            (Expr::operatorToken(a), Expr::operatorToken(b)) => {
+            (intToken(a), intToken(b)) => a == b,
+            (floatToken(a), floatToken(b)) => a == b,
+            (wordToken(a), wordToken(b)) => a == b,
+            (stringToken(a), stringToken(b)) => a == b,
+            (commentToken(a), commentToken(b)) => a == b,
+            (operatorToken(a), operatorToken(b)) => {
                 let mut i = 0;
                 while i < a.len() {
                     match (a[i], b[i]) {
                         (Some(oper1), Some(oper2)) => if oper1 != oper2 { return false },
                         (None, None) => (),
-                        _ => return false;
+                        _ => return false,
                     }
                     i += 1;
                 }
                 return true;
             },
-            (Expr::listExpr(a), Expr::listExpr(b)) => {
-                return true;
+            (listExpr(a), listExpr(b)) => {
+                let mut backtrackA : Vec<(&ListExpr, usize)> = Vec::new();
+                let mut backtrackB : Vec<&ListExpr> = Vec::new();
+                backtrackA.push((a, 0));
+                backtrackB.push(b);
+                let mut i: usize;
+                while backtrackA.last().is_some() {
+                    if backtrackB.last().is_none() { return false }
+                    let mut listA: (&ListExpr, usize) = backtrackA.last().unwrap().clone();
+                    let mut listB: &ListExpr = backtrackB.last().unwrap();
+                    i = listA.1;
+                    if listA.0.pType != listB.pType || listA.0.val.len() != listB.val.len() {
+                        return false;
+                    }
+                    while i < listA.0.val.len() {
+                        let itmA = &listA.0.val[i];
+                        let itmB = &listB.val[i];
+                        match (itmA, itmB) {
+                            (listExpr(itmAList), listExpr(itmBList)) => {
+                                backtrackA.push((listA.0, i + 1));
+                                backtrackB.push(listB);
+                                listA = (&itmAList, 0usize);
+                                listB = &itmBList;
+                                if listA.0.pType != listB.pType || listA.0.val.len() != listB.val.len() {
+                                    return false;
+                                }
+                                i = 0;
+                            },
+                            _ => {
+                                if itmA != itmB { return false }
+                                i += 1
+                            },
+                        }
+                    }
+                }
+                true
             },
+            _ => false,
         }
     }
 }
@@ -39,7 +80,7 @@ impl<'a, 'b> PartialEq for Expr<'b> {
 impl<'a> Eq for Expr<'a> {}
 
 
-#[derive(PartialEq, Copy)]
+#[derive(PartialEq, Clone, Copy)]
 /// & + - / * ! ~ $ % ^ | > < ? =
 enum OperatorSymb {
     ampersand,
@@ -62,7 +103,7 @@ enum OperatorSymb {
 }
 
 
-#[derive(Eq, Copy)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 enum ExprLexicalType {
     statement,
     dataInitializer,
@@ -78,10 +119,10 @@ enum LexError {
     extraClosingBracket,
     nonASCII,
     emptyStack,
-    unexpectedSymbol(&str),
-    intError(&str),
-    floatError(&str),
-    wordError(&str),
-    operatorError(&str),
-    boolError(&str),
+    unexpectedSymbol(String),
+    intError(String),
+    floatError(String),
+    wordError(String),
+    operatorError(String),
+    boolError(String),
 }
--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,9 @@ mod Parser;
 fn main() {
     println!("hello world");
     Lexer::lexicallyAnalyze();
-    let a = LexerTypes::IntToken(5);
+    let a = LexerTypes::Expr::intToken(5);
+    let s: [u8; 3] = [1, 2, 3];
+    let b = LexerTypes::Expr::wordToken(&s);
+    let c = LexerTypes::Expr::wordToken(&s);
+    println!("{}", c == b);
 }
